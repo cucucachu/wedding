ff62ac5ba8928a561e5491dc21fe19473d69ebc5
Cody Jones <codymjones1@gmail.com>
2022-07-31 11:01:30 -0700

    Added program

--- web/src/pages/ProgramPage.jsx
+++ web/src/pages/ProgramPage.jsx
@@ -2,11 +2,34 @@ import React from 'react';
 
 export function ProgramPage() {
     return (
-        <div className="container center-text">
-            <strong>
-                <p className="center-text">Please arrive by 5:00 PM for the ceremony, reception to follow.</p>
-                <p className="center-text">Program to be updated soon, please check back!</p>
-            </strong>
+        <div className="container">
+            
+            <h3 className='center-text'>Program</h3>
+            <div className="schedule-container">
+                <div>
+                    <p>4:15 pm</p>
+                    <p>5:00 pm</p>
+                    <p>5:30 pm</p>
+                    <p>6:30 pm</p>
+                    <p>7:30 pm</p>
+                    <p>7:45 pm</p>
+                    <p>8:00 pm</p>
+                    <p>9:00 pm</p>
+                    <p>11:00 pm</p>
+                </div>
+                <div>
+                    <p>Doors Open</p>
+                    <p>Ceremony</p>
+                    <p>Cocktail Hour</p>
+                    <p>Dinner</p>
+                    <p>First Dance</p>
+                    <p>Dessert</p>
+                    <p>Dancing</p>
+                    <p>Snacks, More Dancing</p>
+                    <p>End of Event</p>
+                    <p></p>
+                </div>
+            </div>
         </div>
     )
 }
